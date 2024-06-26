c4c209d0d398cc67fdff43a3e9f461d413af9a05
Waleed <waleedatif23@gmail.com>
2024-06-26 15:57:27 +0500

    made corrections to initial commit

--- week1/variables.js
+++ week1/variables.js
@@ -10,14 +10,14 @@ var c = "waleed" //javascript string
 var d = true //javascript boolean
 var e = null //javascript null used to show and unintentional empty delaration
 var f = undefined //used to show that the variable is delared but not given the value yet
-var g = //used to show illegal mathematical operation
+var g = 1 / 0 // returns NaN used to show illegal mathematical operation
 
 
-    //in the above examples i have included semicolon and excluded it as well 
-    //semicolon is optional in js but it is a good practice to write it so that you don't forget that in other languages
+//in the above examples i have included semicolon and excluded it as well 
+//semicolon is optional in js but it is a good practice to write it so that you don't forget that in other languages
 
-    //to print this on the terminal we can use 
-    console.log(a, b, c, d) //outputs 10 100000000000 waleed true
+//to print this on the terminal we can use 
+console.log(a, b, c, d) //outputs 10 100000000000 waleed true
 
 
 //following code shows difference between var and let
