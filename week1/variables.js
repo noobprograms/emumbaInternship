e867b14ce7fcd218aa8d396bb84eaf9bdd53924a
Waleed <waleedatif23@gmail.com>
2024-06-26 15:49:03 +0500

    initialized with some variable code

---
+++ week1/variables.js
@@ -0,0 +1,23 @@
+/*this example illustrates that js assigns variable types dynamically according 
+to the value assigned to it.
+
+(this syntax is used to write multiline comments in js)
+*/
+
+var a = 10; //javascript Number
+var b = 10e10 // this is also a Number
+var c = "waleed" //javascript string
+var d = true //javascript boolean
+var e = null //javascript null used to show and unintentional empty delaration
+var f = undefined //used to show that the variable is delared but not given the value yet
+var g = //used to show illegal mathematical operation
+
+
+    //in the above examples i have included semicolon and excluded it as well 
+    //semicolon is optional in js but it is a good practice to write it so that you don't forget that in other languages
+
+    //to print this on the terminal we can use 
+    console.log(a, b, c, d) //outputs 10 100000000000 waleed true
+
+
+//following code shows difference between var and let
