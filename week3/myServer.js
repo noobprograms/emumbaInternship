085dca214436c8a02dde87a43fb3d9278a914c00
Waleed <waleedatif23@gmail.com>
2024-07-03 17:49:31 +0500

    assignment 3 week 3 done

--- week3/myServer.js
+++ week3/myServer.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
 
-app.use(bodyparser());
-app.get('/', myMiddleware, (req, res) => {
+var myOrderList = ['pizza', 'cake', 'burger'];
+app.get('/', myMiddleware, (req, res, next) => {
     console.log(req.body);
     res.send('Hello World');
 });
@@ -17,8 +16,14 @@ app.get('/contact', myMiddleware, (req, res) => {
 app.get('/about', myMiddleware, (req, res) => {
     res.send('this is the place where we give our information');
 });
-app.get('/order', myMiddleware, (req, res) => {
-    res.send('this is the page where to can order stuff');
+app.get('/order', myMiddleware, (req, res, next) => {
+    console.log(req.headers.order);
+    if (myOrderList.includes(req.headers.order)) {
+        res.send('this is the page where to can order stuff');
+
+    } else
+        return next({ message: "we don't have this dish" })
+
 })
 
 function myMiddleware(req, res, next) { //logger middleware for assignment 2
@@ -26,4 +31,9 @@ function myMiddleware(req, res, next) { //logger middleware for assignment 2
 
     return next();
 }
+app.use((error, req, res, next) => {
+    console.error(error.message)
+    res.status(500).send(error.message)
+
+})
 app.listen(8000, () => console.log("myServer running on 8000 port"))
