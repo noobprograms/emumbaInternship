6f21d035a1b75a584e3c922b259fdd4b5ba76719
Waleed <waleedatif23@gmail.com>
2024-07-03 12:56:00 +0500

    more on expressjs routing

--- week2/expresswork/basicServer.js
+++ week2/expresswork/basicServer.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const router = require('./routes/router')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index.ejs', { text: "World" });
 
 });
+
+//in ordrer to use the routes of the router we have defined in the other file we need to mount it inside our app
+app.use("/users", router)
 app.listen(8000, () => console.log("running on port 8000"));
