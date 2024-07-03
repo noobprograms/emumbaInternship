cfdc7b599eee265213277b7b65cab53bb5ff8303
Waleed <waleedatif23@gmail.com>
2024-07-03 16:21:18 +0500

    assignment 2 done of week 3

--- week3/myServer.js
+++ week3/myServer.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
 
-
-app.get('/', (req, res) => {
+app.use(bodyparser());
+app.get('/', myMiddleware, (req, res) => {
+    console.log(req.body);
     res.send('Hello World');
 });
-app.get('/pricing', (req, res) => {
+app.get('/pricing', myMiddleware, (req, res) => {
     res.send('Pricing page');
 });
-app.get('/contact', (req, res) => {
+app.get('/contact', myMiddleware, (req, res) => {
     res.send('this is teh page where we will give our contact information');
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', myMiddleware, (req, res) => {
     res.send('this is the place where we give our information');
 });
-app.get('/order', (req, res) => {
+app.get('/order', myMiddleware, (req, res) => {
     res.send('this is the page where to can order stuff');
 })
 
+function myMiddleware(req, res, next) { //logger middleware for assignment 2
+    console.log(req);
+
+    return next();
+}
 app.listen(8000, () => console.log("myServer running on 8000 port"))
