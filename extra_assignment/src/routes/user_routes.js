45b1a161c7f0587490b91029887370a5b9ffa5de
Waleed <waleedatif23@gmail.com>
2024-07-09 11:55:49 +0500

    added configruations to the gitignore file

--- extra_assignment/src/routes/user_routes.js
+++ extra_assignment/src/routes/user_routes.js
@@ -8,7 +8,7 @@ const jsonParser = bodyParser.json();
 
 userRouter.post('/addUser', jsonParser, addUser);
 
-userRouter.get('/getUser', getUser);
+userRouter.get('/getUser:id', getUser);
 
 userRouter.put('/updateUser/:id', jsonParser, updateUser);
 
